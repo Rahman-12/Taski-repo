a964dce62d9a2c3b67111f8c16dcff4f81e56ff4
ikotun-dev <danlogan2003@gmail.com>
2024-02-19 18:35:48 +0100

    foreign key config

--- newone/core/models.py
+++ newone/core/models.py
@@ -1,80 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
-class Customer(models.Model): 
-    id = models.AutoField(primary_key=True) # incremental, integer 
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)  # incremental, integer
     username = models.CharField(max_length=25, null=True)
     password = models.CharField(max_length=255, null=True)
-    phone_number= models.CharField(max_length=255, null=True)
-    e_mail=models.CharField(max_length=255, null=True)
-    
-
+    phone_number = models.CharField(max_length=255, null=True)
+    e_mail = models.CharField(max_length=255, null=True)
 
 
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=False)
     description = models.CharField(max_length=255, null=False)
-    customer_id = models.ForeignKey("Customer", on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(
+        "Customer", on_delete=models.CASCADE
+    )  # taking the ID of the customer
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
+"""
     CREATE TABLE user ( 
         id INTEGER PRIMARY KEY AUTOINCREMENT
         username VARCHAR(255) NOT NULL
         password VARCHAR(255) NOT NULL
     )
-'''
+"""
